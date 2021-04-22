4fa38b754cd10bbfd79355364f166d6563e18512
KeslieBox <kesliebox@gmail.com>
2021-04-22 15:50:26 -0500

    commit tree 0d2717dea92a23869e80873f4fdf2b525e697c10

--- index.js
+++ index.js
@@ -11,6 +11,43 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+// const titleCased = (tutorials) => {
+//   return tutorials.map(tutorialName => {
+//     const wordsArray = wordsArray.split(" ")
+//     const capitalizedLetter = 
+//       wordsArray.map(word => { 
+//       word.replace(word[0], word[0].toUpperCase())
+//     })
+//     response = capitalizedLetter.join(" ")
+//     return response
+//   })
+// }
+
+const titleCased = (i) => {
+  return tutorials.map(tutorialName => {
+    const wordsArray = tutorialName.split(" ")
+    const capitalizedWords = 
+      wordsArray.map(word => word.replace(word[0], word[0].toUpperCase()))
+    const response = capitalizedWords.join(' ')
+    return response
+  })
 }
+
+// const titleCased = (input) => {
+//   return tutorials.map( line => {
+//     const tokens = line.split(' ');
+//     const capitalizedTokens =
+//       tokens.map( token => token.replace(token[0], token[0].toUpperCase()))
+//     const response = capitalizedTokens.join(' ');
+//     return response;
+//   });
+// }
+
+// const titleCased = (tutorials) => {
+//   return tutorials.map(tutorialName => {
+//    tutorialName.split(" ").map(word => { 
+//       word.replace(word[0], word[0].toUpperCase())
+//     }).join(" ")
+//   })
+// }
